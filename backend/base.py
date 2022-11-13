f93a40f46615b2404d45edeca962e15bfc27297f
Ruize Li <rl3250@columbia.edu>
2022-11-12 21:48:40 -0500

    nit

--- backend/base.py
+++ backend/base.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
-api = Flask(__name__)
+app = Flask(__name__)
 
-@api.route('/profile')
+@app.route('/profile')
 def my_profile():
     response_body = {
         "name": "Nagato",
