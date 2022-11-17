f4de4c40763ae64396bd790b3a706720c2ca7b8e
Ruize Li <rl3250@columbia.edu>
2022-11-17 15:56:01 -0500

    nits

--- src/components/ManageAdsPage.js
+++ src/components/ManageAdsPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from "axios";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -8,16 +8,17 @@ import UserInput from './ads/UserInput';
 import InjectAdsController from './ads/InjectAdsController';
 
 const generateKey = (pre) => {
-    return `${ pre }_${ new Date().getTime() }`;
+    return `${pre}_${new Date().getTime()}`;
 }
 const ManageAdsPage = () => {
     const [feedData, setFeedData] = useState(null)
     const [userInput, setUserInput] = useState(null)
     const [selectedAd, setSelectedAd] = useState(null)
     useEffect(() => {
-        handleFetchData();
+        handleFetchData().then(r => console.log(r));
     }, [])
     const handleInjectAd = async () => {
+        // TODO
         return selectedAd;
     }
     const handleFetchData = async () => {
@@ -31,10 +32,9 @@ const ManageAdsPage = () => {
                 setFeedData(rsp.data);
                 setUserInput(uid);
             })
-            }
-            catch(err) {
-              console.log(err)  
-            }
+        } catch (err) {
+            console.log(err)
+        }
     }
     const feedContent = () => {
         const activities = feedData?.activities?.map((act, index) => (
@@ -43,23 +43,23 @@ const ManageAdsPage = () => {
         return (
             <Col>
                 {activities ?? <h1>FEED with user id NOT FOUND</h1>}
-            </Col>    
+            </Col>
         )
     }
     const header = (userInput) => (
         <>
-        <h1>Manage Ads page</h1>
-        <h3>Showing user feed for uid {userInput}</h3>
+            <h1>Manage Ads page</h1>
+            <h3>Showing user feed for uid {userInput}</h3>
         </>
     );
-    
+
     return (
-    <>
-    {header(userInput)}
-    <UserInput handleOnChange={setUserInput} handleOnSubmit={handleFetchData}/>
-    <InjectAdsController handleOnChange={setSelectedAd} handleOnInject={handleInjectAd}/>
-    {feedData ? feedContent() : <Loading/>}
-    </>
+        <>
+            {header(userInput)}
+            <UserInput handleOnChange={setUserInput} handleOnSubmit={handleFetchData}/>
+            <InjectAdsController handleOnChange={setSelectedAd} handleOnInject={handleInjectAd}/>
+            {feedData ? feedContent() : <Loading/>}
+        </>
     )
 }
 export default ManageAdsPage
