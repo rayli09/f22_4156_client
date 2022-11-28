b2daba88f365476f6863fd714f6c106e13139cd2
Ruize Li <rl3250@columbia.edu>
2022-11-28 12:25:46 -0500

    [fix] fix logout bug

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 export default function Logout(props) {
     props?.setUserData(null);
+    localStorage.removeItem("userData");
     return (
         <Container>
             You've just logged out.
