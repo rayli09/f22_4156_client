a814d6a95c554ae853dee10ab40fb03132a24762
H Lin <34226262+howieraem@users.noreply.github.com>
2022-11-28 14:31:22 -0500

    [fix] prevent user from viewing certain pages if not logged in

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -15,10 +15,14 @@ const MyNavbar = (props) => {
       <Container>
         <Navbar.Brand as={Link} to="/home">Venmo</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link as={Link} to="/feed">Feed</Nav.Link>
-          <Nav.Link as={Link} to="/request">Request</Nav.Link>
-          <Nav.Link as={Link} to="/transfer">Transfer</Nav.Link>
-          {username && <Nav.Link as={Link} to="/profile">Profile</Nav.Link>}
+          {username && (
+            <>
+              <Nav.Link as={Link} to="/feed">Feed</Nav.Link>
+              <Nav.Link as={Link} to="/request">Request</Nav.Link>
+              <Nav.Link as={Link} to="/transfer">Transfer</Nav.Link>
+              <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+            </>
+          )}
          
           {/* {username ||  <Nav.Link as={Link} to="/login">Login</Nav.Link>} */}
           {username ||  <Button as={Link} to="/login">Login</Button>}
